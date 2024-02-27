ee1ecb6b265f7ead0ede33830188fb0db1b635c3
Frere-Luz <algo_de_luz@hotmail.com>
2024-02-27 11:28:51 -0300

    envio cambios minimos y registrarPet.js para revision

--- client/js/registrarPet.js
+++ client/js/registrarPet.js
@@ -1,79 +1,68 @@
-function data() {
-    return {
-        postDataPet: [],
-        url: 'http://127.0.0.1:5000/api/pets/register',
-        // para probar usar localhost 5000
-        // url:'http://hosting/tabla',
-        error: false,
-        cargando: true,
-        /*atributos para el guardar los valores del formulario */
-        id: 0,
-        picture: "",
-        type: "",
-        date_lost: "",
-        location: "",
-        namePet: document.getElementById,
-        breed: "",
-        age: 0,
-        size: "",
-        facebook: "",
-        instagram: "",
-        description: "",
-        hashtags: "",
-    }
+const formulario = document.getElementById('formulario');
+const inputs = document.querySelectorAll('#formulario input');
+
+// Tomo los datos que ingresa el usuario
+
+function registrarPet () {
+    const picturePet = document.getElementById('picturePet').value;
+    const type = document.getElementById('type').value;
+    const date_lost = document.getElementById('date_lost').value;
+    const location = document.getElementById('location').value;
+    const namePet = document.getElementById('namePet').value;
+    const breed = document.getElementById('breed').value;
+    const age = document.getElementById('age').value;
+    const size = document.getElementById('size').value;
+    const facebook = document.getElementById('facebook').value;
+    const instagram = document.getElementById('instagram').value;
+    const description = document.getElementById('description').value;
+    const hashtags = document.getElementById('hashtags').value
+
+    //if (document.document.getElementById("picturePet"!=='.jpg'||'.png'||'jpeg' ) ){
+    //    alert('Error al completar')
+    //}
+
+    // Armo un objeto con los datos a registrar
+
+    const datosPet = {
+        picturePet,
+        type,
+        date_lost,
+        location,
+        namePet,
+        breed,
+        age,
+        size,
+        facebook,
+        instagram,
+        description,
+        hashtags
+    };
+
+// Solicitud de POST
+fetch('http://127.0.0.1:5000/api/pets/register', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(datosPet),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Error en la solicitud Fetch");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log("Respuesta del backend:", data);
+      window.location.href = "/index.html"; // Redirige a index.html
+    })
+    .catch((error) => {
+      console.error("Error al enviar los datos:", error);
+    });
+};
+//vuelve al home si se cancela la operacion
+function cancelarRegistro() {
+    window.location.href = '/index.html';
 };
 
 
-    function fetchData(url) {
-        fetch(url)
-            .then(response => response.json())
-            .then(data => {
-                this.postDataPet = data;
-                this.cargando = false
-            })
-            .catch(err => {
-                console.error(err);
-                this.error = true
-            })
-    };
-    //Solicitud de POST
-    // A los datos ingresados los guardo en una variable que la envio en formato JSON
-    function registrarPet() {
-        var formulario = document.getElementById ('formulario');
-        var namePet = formulario.element["namePet"].value;
-        console.log (namePet);
-        let dataPet = {
-            picture: this.picture,
-            type: this.type,
-            date_lost: this.date_lost,
-            location: this.location,
-            namePet: this.namePet,
-            breed: this.breed,
-            age: this.age,
-            size: this.size,
-            facebook: this.facebook,
-            instagram: this.instagram,
-            description: this.description,
-            hashtags: this.hashtags,
-        }
-        console.log()
-        var options = {
-            body: JSON.stringify(dataPet),
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            redirect: 'follow'
-        }
-        fetch(this.url, options)
-            .then(function () {
-                alert("Registro grabado")
-                window.location.href = "../index.html";
-            })
-            .catch(err => {
-                console.error(err);
-                alert("Error al Grabar")
-            })
-    };
-
-    function saludar (){
-        alert("Hola!")
-    }
