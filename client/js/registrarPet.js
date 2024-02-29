fedbddf8c285eea0df57d6f77d2bbcdbde5ec55d
Frere-Luz <algo_de_luz@hotmail.com>
2024-02-28 21:14:06 -0300

    agrego la visualizacion de la imagen en la creacion del aviso en js, con la modificacion del html

--- client/js/registrarPet.js
+++ client/js/registrarPet.js
@@ -1,52 +1,69 @@
 const formulario = document.getElementById('formulario');
 const inputs = document.querySelectorAll('#formulario input');
 
+// Capturo el archivo que ingresa el usuario y lo muestro por pantalla
+
+const picturePet = document.getElementById('picturePet');
+var uploaded_image = "";
+picturePet.addEventListener('change', function () {
+  const reader = new FileReader();
+  reader.addEventListener('load', () => {
+    uploaded_image = reader.result;
+    document.getElementById("display_image").style.backgroundImage = `url($(uploaded_image))`;
+  })
+  reader.readAsDataURL(this.files[0]);
+});
+
 // Tomo los datos que ingresa el usuario sobre el aviso a publicar
 
 document
-    .getElementById("formulario")
-    .addEventListener("submit", function (event) {
-      console.log("aqui..." + event)
-      event.preventDefault(); // Evita el envío del formulario
+  .getElementById("formulario")
+  .addEventListener("submit", function (event) {
+    console.log("aqui..." + event)
+    event.preventDefault(); // Evita el envío del formulario
 
-      // Registra al aviso
-      registrarPet();
-      //guardarRedes();
-      //resetea el form
-      resetearForm();
-    });
+    // Registra al aviso
+    registrarPet();
+    //guardarRedes(); queda pendiente hasta tener el endpoint de redes sociales
 
-function registrarPet () {
-    const image_url = document.getElementById('picturePet').value;
-    const type = document.getElementById('type').value;
-    const date_lost = document.getElementById('date_lost').value;
-    const location = document.getElementById('location').value;
-    const name = document.getElementById('namePet').value;
-    const breed = document.getElementById('breed').value;
-    const age = document.getElementById('age').value;
-    const size = document.getElementById('size').value;
-    const description = document.getElementById('description').value;
-    const hashtags = document.getElementById('hashtags').value
+    //resetea el form
+    resetearForm();
+  });
 
 
-    // Armo un objeto con los datos  del aviso a registrar
 
-    const datosPet = {
-        image_url,
-        type,
-        date_lost,
-        location,
-        name,
-        breed,
-        age,
-        size,
-        description,
-        //hashtags
-    };
 
-// Solicitud de POST
+function registrarPet() {
+  //const image_url = document.getElementById('picturePet').value;
+  const type = document.getElementById('type').value;
+  const date_lost = document.getElementById('date_lost').value;
+  const location = document.getElementById('location').value;
+  const name = document.getElementById('namePet').value;
+  const breed = document.getElementById('breed').value;
+  const age = document.getElementById('age').value;
+  const size = document.getElementById('size').value;
+  const description = document.getElementById('description').value;
+  const hashtags = document.getElementById('hashtags').value
 
-fetch("http://127.0.0.1:5000/api/pets/", {
+
+  // Armo un objeto con los datos  del aviso a registrar
+
+  const datosPet = {
+    image_url,
+    type,
+    date_lost,
+    location,
+    name,
+    breed,
+    age,
+    size,
+    description,
+    //hashtags
+  };
+
+  // Solicitud de POST
+
+  fetch("http://127.0.0.1:5000/api/pets/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -62,7 +79,7 @@ fetch("http://127.0.0.1:5000/api/pets/", {
     .then((data) => {
       console.log("Respuesta del backend:", data);
       window.location.href = "../index.html"; // Redirige al home
-      
+
     })
     .catch((error) => {
       console.error("Error al enviar los datos:", error);
@@ -101,11 +118,11 @@ fetch("http://127.0.0.1:5000/api/pets/", {
     });
 }*/
 
-function resetearForm(){
+function resetearForm() {
   document.getElementById("formulario").reset();
 }
 
 //vuelve al home si se cancela la operacion
 function cancelarRegistro() {
-    window.location.href = '../index.html';
+  window.location.href = '../index.html';
 };
