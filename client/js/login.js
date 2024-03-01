4dda02c5c46b60e5c626baf11c540fcea698a71e
Maria Soledad Fornasier <informatica.ijo@gmail.com>
2024-03-01 17:39:17 -0300

    logueo con guardado de toquen

--- client/js/login.js
+++ client/js/login.js
@@ -1,30 +1,40 @@
-document
-  .getElementById("loginForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault(); // Evitar el envío predeterminado del formulario
+document.getElementById("loginForm").addEventListener("submit", function (event) {
+  event.preventDefault();
 
-    // Obtener los valores del formulario
-    var username = document.getElementById("user_log").value.trim();
-    var password = document.getElementById("psw_log").value.trim();
+  var username = document.getElementById("user_log").value.trim();
+  var password = document.getElementById("psw_log").value.trim();
 
-    // Enviar los datos al servidor  para autenticar al usuario
-    fetch("http://127.0.0.1:5000/api/auth/login", {
+  fetch("http://127.0.0.1:5000/api/auth/login", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json",
+          "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        username: username,
-        password: password,
+          username: username,
+          password: password,
       }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        //Guardo el token en el sessionStorage
-        let token = data.access_token;
-        sessionStorage.setItem("token", token);
-      })
-      .catch((error) => {
-        console.error("Error en la solicitud:", error);
-      });
+  })
+  .then((response) => {
+      if (!response.ok) {
+          if (response.status === 401) {
+              throw new Error("Nombre de usuario o contraseña incorrecta");
+          }
+          throw new Error("Error en la solicitud. Estado: " + response.status);
+      }
+      return response.json();
+  })
+  .then((data) => {
+      let token = data.access_token;
+      console.log(token);
+      localStorage.setItem("token", token);
+      window.location.href = "../html/logued.html";
+  })
+  .catch((error) => {
+      console.error("Error en la solicitud:", error.message);
+      alert(error.message);
+      
+      // Limpiar el formulario después de aceptar el alert
+      document.getElementById("user_log").value = "";
+      document.getElementById("psw_log").value = "";
   });
+});
